c648df2d6a8c59837ae807e9a8d332f171bf5f33
AURET Gabriel <GAURO7KZ@eu.airbus.corp>
2023-11-30 16:06:29 +0100

    test

--- node-hello-world/routes/index.js
+++ node-hello-world/routes/index.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
   res.send(
     { "greeting": "Hello, world!" }
   );
 });
+
+// router.get('/', function(req, res, next) {
+//   res.send(
+//     { "greeting": "Hello, world!" }
+//   );
+// });
 
 module.exports = router;
